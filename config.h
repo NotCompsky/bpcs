b395a0996841a6101a61bc71a3903f6eb9dc60f9
Compsky <adammarkgray@gmail.com>
2019-04-02 17:20:09 +0100

    Fix: Restrict header definitions with #ifndef branches

--- config.h
+++ config.h
@@ -1,2 +1,5 @@
+#ifndef BPCS_CONFIG
 #define N_BITPLANES 8
 #define N_CHANNELS 3
+#define BPCS_CONFIG
+#endif
--- units.h
+++ units.h
@@ -1,5 +1,8 @@
+#ifndef BPCS_UNITS
 typedef unsigned char uchar;
 typedef __uint8_t uint8_t;
 typedef __int32_t int32_t;
 typedef __uint32_t uint32_t;
 typedef __uint64_t uint64_t;
+#define BPCS_UNITS
+#endif
