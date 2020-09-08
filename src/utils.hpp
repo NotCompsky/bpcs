684c5e79c296121740e3bbc99d6ea404600f00c3
NotCompsky <adammarkgray@gmail.com>
2020-09-08 13:21:28 +0100

    Fix/Tidy/Performance		Remove memory allocations etc.

--- src/utils.hpp
+++ src/utils.hpp
@@ -8,7 +8,8 @@
 #endif
 
 
-inline int32_t _format_out_fp(char* out_fmt, char** fp, int32_t dir_length, int32_t indx_dot, int32_t i, int32_t fp_length){
+inline
+void _format_out_fp(char* out_fmt, char* fp, char result[1024], int32_t dir_length, int32_t indx_dot, int32_t i, int32_t fp_length){
     int32_t basename_length;
     int32_t ext_length = i - indx_dot;
     int32_t fname_length = i - dir_length;
@@ -21,46 +22,18 @@ inline int32_t _format_out_fp(char* out_fmt, char** fp, int32_t dir_length, int3
         ext_length = 0;
     }
     
-    int32_t result_length = 0;
-    
-    for (char* it=out_fmt; *it!=0; ++it){
-        if (*it == '{'){
-            switch(*(++it)){
-                case '{': goto append_ch;
-                case 'b':         result_length+=basename_length;   it+=8; break;
-                case 'd':         result_length+=dir_length;        it+=3; break;
-                case 'e':         result_length+=ext_length;        it+=3; break;
-                case 'f':
-                    switch(*(++it)){
-                        case 'p': result_length+=fp_length;         it+=1; break;
-                        default:  result_length+=fname_length;      it+=4; break;
-                    }
-                    break;
-            }
-#ifdef TESTS
-            assert(*it == '}');
-#endif
-            continue;
-        }
-        append_ch:
-        result_length += 1;
-    }
-    ++result_length; // terminating 0
-    
-    char* result = (char*)malloc(result_length);
-    
     int32_t j = 0;
     for (char* it=out_fmt;  *it!=0;  ++it){
         if (*it == '{'){
             switch(*(++it)){
                 case '{': result[j]='{'; goto count_ch;
-                case 'b': memcpy(result + j, *fp + dir_length + 1, basename_length);    j+=basename_length; it+=8; break;
-                case 'd': memcpy(result + j, *fp, dir_length);                          j+=dir_length;      it+=3; break;
-                case 'e': memcpy(result + j, *fp + indx_dot + 1, ext_length);           j+=ext_length;      it+=3; break;
+                case 'b': memcpy(result + j, fp + dir_length + 1, basename_length);    j+=basename_length; it+=8; break;
+                case 'd': memcpy(result + j, fp, dir_length);                          j+=dir_length;      it+=3; break;
+                case 'e': memcpy(result + j, fp + indx_dot + 1, ext_length);           j+=ext_length;      it+=3; break;
                 case 'f':
                     switch(*(++it)){
-                        case 'p': memcpy(result + j, *fp, fp_length);                   j+=fp_length;       it+=1; break;
-                        default: memcpy(result + j, *fp + dir_length +1, fname_length); j+=fname_length;    it+=4; break;
+                        case 'p': memcpy(result + j, fp, fp_length);                   j+=fp_length;       it+=1; break;
+                        default: memcpy(result + j, fp + dir_length +1, fname_length); j+=fname_length;    it+=4; break;
                     }
                     break;
             }
@@ -71,21 +44,18 @@ inline int32_t _format_out_fp(char* out_fmt, char** fp, int32_t dir_length, int3
         ++j;
     }
     
-    result[result_length - 1] = 0;
-    
-    *fp = result;
-    
-    return result_length;
+	result[j] = 0;
 }
 
-int32_t format_out_fp(char* out_fmt, char** fp){
+inline
+void format_out_fp(char* out_fmt,  char* fp,  char result[1024]){
     // WARNING: Requires absolute paths?
     
     int32_t dir_length = 0;
     int32_t indx_dot = 0;
     int32_t i = 0;
     
-    for (char* it=*fp; true; ++it, ++i){
+    for (char* it=fp; true; ++it, ++i){
         switch(*it){
             case '/': dir_length=i; break;
             case '.': indx_dot=i; break;
@@ -96,23 +66,5 @@ int32_t format_out_fp(char* out_fmt, char** fp){
     
     --i;
     
-    return _format_out_fp(out_fmt, &(*fp), dir_length, indx_dot, i, i+1);
-}
-
-int32_t format_out_fp(char* out_fmt, char** fp, uint64_t fp_length){
-    // WARNING: Requires absolute paths?
-    
-    int32_t dir_length = 0;
-    int32_t indx_dot = 0;
-    int32_t i = 0; // eventual value will be fp_length - 1
-    
-    for (char* it=*fp; i<fp_length; ++i){
-        switch(*(it++)){
-            case '/': dir_length=i; break;
-            case '.': indx_dot=i; break;
-        }
-    }
-    --i;
-    
-    return _format_out_fp(out_fmt, &(*fp), dir_length, indx_dot, i, fp_length);
+	_format_out_fp(out_fmt, fp, result, dir_length-1, indx_dot, i, i+1);
 }
