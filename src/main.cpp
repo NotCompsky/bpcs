996ea8f0d7b86ddf74d94b4a285e938f04c84e3e
NotCompsky <adammarkgray@gmail.com>
2020-09-18 16:24:30 +0100

    Tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include "bpcs.hpp"
+#include "os.hpp"
 #include "errors.hpp"
 #include <compsky/macros/likely.hpp>
 #include <unistd.h> // for STD(IN|OUT)_FILENO
-
-
-#define WHILE_CONDITION while(not bpcs_stream.exhausted)
-#ifdef ONLY_COUNT
-# define DO_OR_WHILE WHILE_CONDITION
-# define WHILE_OR_DO
-#else
-# define DO_OR_WHILE do
-# define WHILE_OR_DO WHILE_CONDITION;
-#endif
 
 
 int main(const int argc, char* argv[]){
@@ -47,36 +38,14 @@ int main(const int argc, char* argv[]){
                               #endif
                               );
     bpcs_stream.load_next_img(); // Init
-	
-	uchar io_buf[((1024 * 64) / BYTES_PER_GRID) * BYTES_PER_GRID]; // Ensure it is divisible by BYTES_PER_GRID
-	uchar* io_buf_itr = io_buf;
     
 #ifdef EMBEDDOR
   if (!embedding){
 #endif
-    DO_OR_WHILE {
-		bpcs_stream.get(io_buf_itr);
-		io_buf_itr += BYTES_PER_GRID;
-#ifdef ONLY_COUNT
-		count += BYTES_PER_GRID;
-#endif
-		if (unlikely((io_buf_itr == io_buf + sizeof(io_buf)) or (bpcs_stream.exhausted))){
-#ifndef ONLY_COUNT
-			if (unlikely(write(STDOUT_FILENO, io_buf, sizeof(io_buf)) != sizeof(io_buf)))
-				break;
-#endif
-			io_buf_itr = io_buf;
-		}
-    } WHILE_OR_DO
+	os::extract_to_stdout(bpcs_stream);
 #ifdef EMBEDDOR
   } else {
-	while (likely(read(STDIN_FILENO, io_buf, BYTES_PER_GRID) == BYTES_PER_GRID)){
-		// TODO: Optimise
-        // read() returns the number of bytes written
-		bpcs_stream.put(io_buf);
-	}
-	bpcs_stream.put(io_buf);
-    bpcs_stream.save_im();
+	os::embed_from_stdin(bpcs_stream);
   }
 #endif
 #ifdef ONLY_COUNT
---
+++ src/os.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+
+#define WHILE_CONDITION while(not bpcs_stream.exhausted)
+#ifdef ONLY_COUNT
+# define DO_OR_WHILE WHILE_CONDITION
+# define WHILE_OR_DO
+#else
+# define DO_OR_WHILE do
+# define WHILE_OR_DO WHILE_CONDITION;
+#endif
+
+
+namespace os {
+
+
+void extract_to_stdout(BPCSStreamBuf& bpcs_stream){
+	static uchar io_buf[((1024 * 64) / BYTES_PER_GRID) * BYTES_PER_GRID]; // Ensure it is divisible by BYTES_PER_GRID
+	uchar* io_buf_itr = io_buf;
+	DO_OR_WHILE {
+		bpcs_stream.get(io_buf_itr);
+		io_buf_itr += BYTES_PER_GRID;
+#ifdef ONLY_COUNT
+		count += BYTES_PER_GRID;
+#endif
+		if (unlikely((io_buf_itr == io_buf + sizeof(io_buf)) or (bpcs_stream.exhausted))){
+#ifndef ONLY_COUNT
+			if (unlikely(write(STDOUT_FILENO, io_buf, sizeof(io_buf)) != sizeof(io_buf)))
+				break;
+#endif
+			io_buf_itr = io_buf;
+		}
+	} WHILE_OR_DO
+}
+
+
+#ifdef EMBEDDOR
+void embed_from_stdin(BPCSStreamBuf& bpcs_stream){
+	static uchar io_buf[((1024 * 64) / BYTES_PER_GRID) * BYTES_PER_GRID]; // Ensure it is divisible by BYTES_PER_GRID
+	uchar* io_buf_itr = io_buf;
+	while (likely(read(STDIN_FILENO, io_buf, BYTES_PER_GRID) == BYTES_PER_GRID)){
+		// TODO: Optimise
+		bpcs_stream.put(io_buf);
+	}
+	bpcs_stream.put(io_buf);
+    bpcs_stream.save_im();
+}
+#endif
+
+
+} // namespace os
