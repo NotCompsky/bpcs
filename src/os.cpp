0172871a0694aa9c029c78df8258cb1496a16b6d
NotCompsky <adammarkgray@gmail.com>
2020-09-20 16:53:26 +0100

    Tidy		Reduce use of macros.

--- src/os.cpp
+++ src/os.cpp
@@ -4,28 +4,28 @@
 #endif
 
 
-#ifdef _WIN32
 bool write_to_stdout(uchar io_buf[IO_BUF_SZ],  const size_t n_bytes){
+  #ifdef _WIN32
 	LPDWORD n_bytes_read_ptr;
 	if (unlikely(WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), io_buf, n_bytes, n_bytes_read_ptr, nullptr) == 0))
 		handler(CANNOT_WRITE_TO_STDOUT);
 	return (*n_bytes_read_ptr != n_bytes);
+  #else
+	return (write(STDOUT_FILENO, io_buf, n_bytes) != n_bytes);
+  #endif
 }
-# define WRITE_STMT write_to_stdout(io_buf)
 
 
 bool read_from_stdin(uchar io_buf[IO_BUF_SZ]){
+  #ifdef _WIN32
 	LPDWORD n_bytes_read_ptr;
 	if (unlikely(ReadFile(GetStdHandle(STD_INPUT_HANDLE), io_buf, BYTES_PER_GRID, n_bytes_read_ptr, nullptr) == 0))
 		handler(CANNOT_READ_FROM_STDIN);
-	return (*n_bytes_read_ptr == BYTES_PER_GRID);
+	return (*n_bytes_read_ptr != BYTES_PER_GRID);
+  #else
+	return (read(STDIN_FILENO, io_buf, BYTES_PER_GRID) != BYTES_PER_GRID);
+  #endif
 }
-# define READ_STMT  read_from_stdin(io_buf)
-#else
-# include <unistd.h>
-# define WRITE_STMT write(STDOUT_FILENO, io_buf, sizeof(io_buf)) != sizeof(io_buf)
-# define READ_STMT  read (STDIN_FILENO, io_buf, BYTES_PER_GRID) == BYTES_PER_GRID
-#endif
 
 
 namespace os {
@@ -58,7 +58,7 @@ size_t extract_to_stdout(BPCSStreamBuf& bpcs_stream,  uchar io_buf[IO_BUF_SZ]){
 #ifdef EMBEDDOR
 void embed_from_stdin(BPCSStreamBuf& bpcs_stream,  uchar io_buf[IO_BUF_SZ]){
 	uchar* io_buf_itr = io_buf;
-	while (likely(READ_STMT)){
+	while (likely(not read_from_stdin(io_buf))){
 		// TODO: Optimise
 		bpcs_stream.put(io_buf);
 	}
